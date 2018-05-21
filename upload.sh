2076f4e1a85bcb19385c89f32acd66641d4ef5cc
B3N50N <b900613@gmail.com>
2018-05-21 12:44:50 -0400

    change file name

---
+++ upload.sh
@@ -0,0 +1,5 @@
+git pull https://github.com/B3N50N/seamcarving-detect.git
+git add *
+git commit -m "change file name"
+git remote add origin https://github.com/B3N50N/seamcarving-detect.git
+git push -u origin master 
