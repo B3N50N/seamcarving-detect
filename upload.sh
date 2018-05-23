2b9b9ddc9505772df5a6149a78e54ec86b010878
B3N50N <b900613@gmail.com>
2018-05-23 07:33:31 -0400

    add scaler for label && edit network(change to tf.tanh)

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git pull https://github.com/B3N50N/seamcarving-detect.git
 git add *
-git commit -m "upload all file"
+git commit -m "add scaler for label && edit network(change to tf.tanh)"
 git remote add origin https://github.com/B3N50N/seamcarving-detect.git
 git push -u origin master 
